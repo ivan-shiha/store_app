f0b882e419b8f88914811d29bedc616b2acc4c46
Ivan Shiha <ivanshiha@gmail.com>
2020-03-03 14:47:08 +0200

    create and register animal model

--- store/animals/models.py
+++ store/animals/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Animal(models.Model):
+    KIND_CHOISES = {
+        ('D', 'Dog'),
+        ('C', 'Cat')
+    }
+    name = models.CharField(max_length=20)
+    age = models.PositiveIntegerField()
+    breed = models.CharField(max_length=20)
+    description = models.TextField()
+    image_url = models.URLField()
+    kind = models.CharField(max_length=1, choices=KIND_CHOISES)
+
+    def __str__(self):
+        return f'{self.name} and I am {self.age}'
